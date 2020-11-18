fc9acfffeb3a5e4f257b9fe67d833ad751891012
Ivan <64037797+CtrAtlDel@users.noreply.github.com>
2020-11-19 01:34:04 +0300

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,69 @@
+import hashlib
+import json
+from time import time 
+from hashlib import sha256
+
+x = 5
+y = 0
+while sha256(f'{x * y}'.encode()).hexdigest()[-1] != 0
+	y += 1
+print(f'The solution is y = {y}') 
+
+class Blockchain(object):
+	def __init__(self):
+		self.chain = []
+		serlf.current_transaction = []
+
+		self.new_block(previous_hash = 1, proof = 100)
+
+
+	def new_block(self):
+		block = {
+			'index' : len(self.chain) + 1,
+			'timestamp' : time(),
+			'transactions' : self.current_transaction,
+			'proof' : proof,
+			'previous_hash' : previous_hash or self.hash(self.chain[-1]),
+		}
+
+		self.current_transaction = []
+
+		self.chain.append(block)
+
+		return block
+
+
+	def new_transaction():
+		self.current_transaction.append({
+		'sender':sender,
+		'recipient':recipient,
+		'amount': amount,
+		})
+
+		return self.last_block['index'] + 1
+
+
+	@staticmethod
+	def hash(block):
+		block_string = json.dumps(block, sort_keys = True).encode()
+		return hashlib.sha256(block_string).hexdigest()
+
+
+	@property
+	def last_block(self):
+		return self.chain[-1]
+
+#This is example of block 
+#block = {
+#   'index': 1,
+#   'timestamp': 1506057125.900785,
+#   'transactions': [
+#       {
+#            'sender': "8527147fe1f5426f9dd545de4b27ee00",
+#            'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+#            'amount': 5,
+#        }
+#    ],
+#    'proof': 324984774000,
+#    'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+#}
